4a8c6db33af0c742b40b6e07f0755c96583917c0
Raoul <50175388+HariboInc@users.noreply.github.com>
2023-03-11 00:37:37 +0100

    feat(locales/nl.lua) Dutch translations

---
+++ locales/nl.lua
@@ -0,0 +1,15 @@
+Locales["nl"] = {
+    toggleHud = 'Zet HUD aan / uit',
+    indicatorLeft = 'Linker knipperlicht',
+    indicatorRight = 'Rechter knipperlicht',
+    toggleEngine = 'Motor aan / uit',
+    speedChanged = 'Snelheid veranderd, nieuwe snelheid: ',
+    resource_not_built = 'Resource is niet gebouwd, download de release versie',
+    settingsSaveSuccess = "Opties successvol opgeslagen",
+    settingsResetSuccess = "Successvol de opties gereset",
+    currentVersion = "Laatste versie: ",
+    yourVersion = "Jou versie: ",
+    latestVersion = "Je gebruikt de laatste versie!",
+    needUpdateResource = "Er is een nieuwe versie beschikbaar, je gebruikt momenteel een verouderde versie!",
+    errorGetCurrentVersion = "Er is een fout opgetreden bij het ophalen van de versie!"
+}
