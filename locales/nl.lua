7c170cb2268573cf796fbcf1f9fafa7300ee2a3d
Arctos2win <116841243+Arctos2win@users.noreply.github.com>
2023-03-11 17:10:11 +0100

    Create nl.lua
    Added Dutch translation

---
+++ locales/nl.lua
@@ -0,0 +1,15 @@
+Locales["nl"] = {
+    toggleHud = 'Zet HUD aan / uit',
+    indicatorLeft = 'Linker richtingaanwijzer',
+    indicatorRight = 'Rechter richtingaanwijzer',
+    toggleEngine = 'Motor aan / uit',
+    speedChanged = 'Snelheid veranderd, nieuwe snelheid: ',
+    resource_not_built = 'Resource is niet gebouwd, download de releaseversie',
+    settingsSaveSuccess = "De instellingen zijn succesvol opgeslagen",
+    settingsResetSuccess = "De instellingen zijn succesvol gereset",
+    currentVersion = "Laatste versie: ",
+    yourVersion = "Jouw versie: ",
+    latestVersion = "Je gebruikt de laatste versie !",
+    needUpdateResource = "Je bent een oude versie aan het gebruiken ! download de nieuwste versie !",
+    errorGetCurrentVersion = "Er is een error ontstaan tijdens het nakijken van de huidige versie !"
+}
