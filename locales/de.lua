b774c9394fb225c6cf3321499dcd7f576264189c
Lenny822 <84620158+Lenny822@users.noreply.github.com>
2023-04-15 12:22:36 +0200

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,15 @@
+Locales["de"] = {
+    toggleHud = 'HUD An / Ausschalten',
+    indicatorLeft = 'Linker Blinker',
+    indicatorRight = 'Rechter Blinker',
+    toggleEngine = 'Motor an / aus',
+    speedChanged = 'Geschwindigkeit geändert! Neue Geschwindigkeit: ',
+    resource_not_built = 'Resource ist gebuilded! Downloade die neuste version!',
+    settingsSaveSuccess = "Erfolgreich die derzeitigen einstellungen gespeichert!",
+    settingsResetSuccess = "Einstellungen erfolgreich zurückgesetzt",
+    currentVersion = "Letzte Version: ",
+    yourVersion = "Deine Version: ",
+    latestVersion = "Du nutzt die Letze Version!",
+    needUpdateResource = "Du musst die letze Version downloaden! Installiere die neuste Version!",
+    errorGetCurrentVersion = "Ein Fehler kam als die Versionsabfrage durchgeführt wurde!"
+}
