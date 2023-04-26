ad2b73ddab41454b22c62386011d6b664684c48f
Slewog <31140215+Slewog@users.noreply.github.com>
2023-04-27 00:18:07 +0200

    Add french translations
    First part of addition of french translations

---
+++ locales/fr.lua
@@ -0,0 +1,15 @@
+Locales["fr"] = {
+    toggleHud = 'HUD on / off',
+    indicatorLeft = 'Clignotant gauche',
+    indicatorRight = 'Clignotant droit',
+    toggleEngine = 'Moteur on / off',
+    speedChanged = 'Vitesse changée, nouvelle vitesse: ',
+    resource_not_built = 'La ressource n\'est pas build, télécharger la release ou compilé celle-ci',
+    settingsSaveSuccess = "Les paramètres actuels ont bien été sauvegarder",
+    settingsResetSuccess = "Paramètres réinitialiser avec succès",
+    currentVersion = "Dernière version: ",
+    yourVersion = "Votre version version: ",
+    latestVersion = "Vous utilisez actuellement la dernière version!",
+    needUpdateResource = "Vous devez télécharger la dernière version ! Vous utilisez une ancienne version pour le moment!",
+    errorGetCurrentVersion = "Une erreur s'est produite lors de la tentative d'obtention de la version actuelle!"
+}
