cb2dd7633b2bad03c69c9c2166e55ef7ecedd0f4
nikartik <110419961+NikartikTheBear@users.noreply.github.com>
2023-04-29 11:58:41 +0200

    fix(locale/it) format correction

--- locales/it.lua
+++ locales/it.lua
@@ -4,12 +4,12 @@ Locales["it"] = {
     indicatorRight = 'Indicatore destro',
     toggleEngine = 'Motore acceso/spento',
     speedChanged = 'Velocità modificata, nuova velocità ',
-    resource_not_built = 'La risorsa non è stata creata, scarica la versione di rilascio',
+    resource_not_built = 'La risorsa non è stata buildata, scarica la versione release, non il soource code!',
     settingsSaveSuccess = "Salvataggio delle impostazioni correnti riuscito",
     settingsResetSuccess = "Ripristino impostazioni riuscito",
     currentVersion = "Ultima versione ",
     yourVersion = "La tua versione:",
-    latestVersion = "Stai utilizzando l'ultima versione!",
-    needUpdateResource = "Devi scaricare l'ultima versione! Al momento stai usando una vecchia versione!",
+    latestVersion = "Stai utilizzando l\'ultima versione!",
+    needUpdateResource = "Devi scaricare l\'ultima versione! Al momento stai usando una vecchia versione!",
     errorGetCurrentVersion = "Si è verificato un errore durante il tentativo di ottenere la versione corrente!"
 }
