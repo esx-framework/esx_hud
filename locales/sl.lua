90cb1a5a23f65ef0eba8912b4fc63cf1ec8791c9
C3h1c <fivemsloofficial@gmail.com>
2023-06-10 17:51:47 +0200

    sl.lua

---
+++ locales/sl.lua
@@ -0,0 +1,15 @@
+Locales["sl"] = {
+    toggleHud = 'HUD on/off',
+    indicatorLeft = 'Lev Smernik',
+    indicatorRight = 'Desni Smernik',
+    toggleEngine = 'Masina on/off',
+    speedChanged = 'HItrost spremenjena, trenutna hitrost: ',
+    resource_not_built = 'Scripta ni buildana, prosimo da jo nalozite s najnovejsim Updatom.',
+    settingsSaveSuccess = "Uspesno shranjena trenutna nastavitva",
+    settingsResetSuccess = "Uspesno resetirane nastavitve",
+    currentVersion = "Najnovejsa verzija: ",
+    yourVersion = "Vasa verzija: ",
+    latestVersion = "Vi uporabljate najnovejso verzijo!",
+    needUpdateResource = "Vi rabite downloadati najnovejso verzijo skripte! Trenutno imate staro!",
+    errorGetCurrentVersion = "Med poskusom pridobitve trenutne različice je prišlo do napake!"
+}
