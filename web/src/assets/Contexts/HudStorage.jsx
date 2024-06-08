46714a5ef12458025954014d310f9715ea855c11
Kue Kering <127606753+Rangerhitam12@users.noreply.github.com>
2024-06-08 15:51:12 +0700

    Update HudStorage.jsx

--- web/src/assets/Contexts/HudStorage.jsx
+++ web/src/assets/Contexts/HudStorage.jsx
@@ -42,6 +42,18 @@ const initialState = {
             progressLevel: 100,
             color: "green",
             icon: StaminaIcon
+        },
+        {
+            name: "stressBar",
+            progressLevel: 100,
+            color: "orange",
+            icon: StressIcon // Pastikan Anda memiliki ikon untuk stress
+        },
+        {
+            name: "drunkBar",
+            progressLevel: 100,
+            color: "purple",
+            icon: DrunkIcon // Pastikan Anda memiliki ikon untuk drunk
         }
     ],
     speedo:{
