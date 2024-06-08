a9d4f951b8a2065321d403058e1a9a05d696c14f
Kue Kering <127606753+Rangerhitam12@users.noreply.github.com>
2024-06-08 16:18:38 +0700

    Update status.lua

--- client/player/status.lua
+++ client/player/status.lua
@@ -3,13 +3,16 @@ if not Config.Disable.Status then
     local playerId = PlayerId()
 
     AddEventHandler("esx_status:onTick", function(data)
-        local hunger, thirst
+        local hunger, thirst, stress
         for i = 1, #data do
             if data[i].name == "thirst" then
                 thirst = math.floor(data[i].percent)
             end
             if data[i].name == "hunger" then
                 hunger = math.floor(data[i].percent)
+            end
+            if data[i].name == "stress" then
+                stress = math.floor(data[i].percent)
             end
         end
 
@@ -19,26 +22,32 @@ if not Config.Disable.Status then
         values.armorBar = GetPedArmour(ped)
         values.drinkBar = thirst
         values.foodBar = hunger
+        values.stressBar = stress
     end)
 
     function HUD:StatusThread()
-        values = {}
-        CreateThread(function()
-            while ESX.PlayerLoaded do
-                local oxygen, stamina
-                oxygen = math.floor(GetPlayerUnderwaterTimeRemaining(playerId) * 10)
-                stamina = math.floor(100 - GetPlayerSprintStaminaRemaining(playerId))
-                if stamina == 0 then
-                    stamina = 1
-                end
-                if stamina == 100 then
-                    stamina = 0
-                end
-                values.oxygenBar = IsPedSwimmingUnderWater(PlayerPedId()) and oxygen or 0
-                values.staminaBar = stamina
-                SendNUIMessage({ type = "STATUS_HUD", value = values })
-                Wait(200)
+    values = {}
+    CreateThread(function()
+        while ESX.PlayerLoaded do
+            local oxygen, stamina, drunk
+            oxygen = math.floor(GetPlayerUnderwaterTimeRemaining(playerId) * 10)
+            stamina = math.floor(100 - GetPlayerSprintStaminaRemaining(playerId))
+            if stamina == 0 then
+                stamina = 1
             end
-        end)
-    end
+            if stamina == 100 then
+                stamina = 0
+            end
+
+            -- Add drunk status calculation here
+            drunk = math.floor(ESX.Game.GetPlayerDrunkLevel(playerId) or 0)  -- Assuming there is a method to get the player's drunk level
+
+            values.oxygenBar = IsPedSwimmingUnderWater(PlayerPedId()) and oxygen or 0
+            values.staminaBar = stamina
+            values.drunkBar = drunk  -- Add drunk level to the values table
+            SendNUIMessage({ type = "STATUS_HUD", value = values })
+            Wait(200)
+        end
+    end)
 end
+
