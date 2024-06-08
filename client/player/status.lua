33360fb8232c7a27c43797f26c17e460c36dc178
Kue Kering <127606753+Rangerhitam12@users.noreply.github.com>
2024-06-08 19:49:17 +0700

    Update status.lua

--- client/player/status.lua
+++ client/player/status.lua
@@ -22,31 +22,35 @@ if not Config.Disable.Status then
         values.armorBar = GetPedArmour(ped)
         values.drinkBar = thirst
         values.foodBar = hunger
-        values.stressBar = stress
+        if not Config.Disable.Stress then
+            values.stressBar = stress
+        end
     end)
 
     function HUD:StatusThread()
-    values = {}
-    CreateThread(function()
-        while ESX.PlayerLoaded do
-            local oxygen, stamina, drunk
-            oxygen = math.floor(GetPlayerUnderwaterTimeRemaining(playerId) * 10)
-            stamina = math.floor(100 - GetPlayerSprintStaminaRemaining(playerId))
-            if stamina == 0 then
-                stamina = 1
-            end
-            if stamina == 100 then
-                stamina = 0
-            end
+        values = {}
+        CreateThread(function()
+            while ESX.PlayerLoaded do
+                local oxygen, stamina, drunk
+                oxygen = math.floor(GetPlayerUnderwaterTimeRemaining(playerId) * 10)
+                stamina = math.floor(100 - GetPlayerSprintStaminaRemaining(playerId))
+                if stamina == 0 then
+                    stamina = 1
+                end
+                if stamina == 100 then
+                    stamina = 0
+                end
 
-            -- Add drunk status calculation here
-            drunk = math.floor(ESX.Game.GetPlayerDrunkLevel(playerId) or 0)  -- Assuming there is a method to get the player's drunk level
+                -- Add drunk status calculation here
+                drunk = math.floor(ESX.Game.GetPlayerDrunkLevel(playerId) or 0)  -- Assuming there is a method to get the player's drunk level
 
-            values.oxygenBar = IsPedSwimmingUnderWater(PlayerPedId()) and oxygen or 0
-            values.staminaBar = stamina
-            values.drunkBar = drunk  -- Add drunk level to the values table
-            SendNUIMessage({ type = "STATUS_HUD", value = values })
-            Wait(200)
-        end
-    end)
-   end
+                values.oxygenBar = IsPedSwimmingUnderWater(PlayerPedId()) and oxygen or 0
+                values.staminaBar = stamina
+                values.drunkBar = drunk  -- Add drunk level to the values table
+
+                SendNUIMessage({ type = "STATUS_HUD", value = values })
+                Wait(200)
+            end
+        end)
+    end
+end
