c3062fcec284430a27762459cb53b009bf03bd5e
Arctos2win <Arctos2win@users.noreply.github.com>
2024-09-16 13:39:36 +0000

    :art:Code formatted in *.lua files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,38 +1,38 @@
-fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
+fx_version("cerulean")
+game("gta5")
+lua54("yes")
 
-description 'The default HUD resource for ESX-Legacy.'
-version '1.8.0'
+description("The default HUD resource for ESX-Legacy.")
+version("1.8.0")
 
-shared_scripts {
-    '@es_extended/imports.lua',
-    '@es_extended/locale.lua',
-    'shared/config.lua',
-    'shared/main.lua',
-    'locales/*.lua'
-}
+shared_scripts({
+    "@es_extended/imports.lua",
+    "@es_extended/locale.lua",
+    "shared/config.lua",
+    "shared/main.lua",
+    "locales/*.lua",
+})
 
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server/*.lua'
-}
+server_scripts({
+    "@oxmysql/lib/MySQL.lua",
+    "server/*.lua",
+})
 
-client_scripts {
-    'client/*.lua',
-    'client/player/*.lua',
-    'client/vehicle/*.lua'
-}
+client_scripts({
+    "client/*.lua",
+    "client/player/*.lua",
+    "client/vehicle/*.lua",
+})
 
-ui_page 'web/dist/index.html'
+ui_page("web/dist/index.html")
 
-files {
-    'web/dist/**',
-    'web/dist/assets/**',
-}
+files({
+    "web/dist/**",
+    "web/dist/assets/**",
+})
 
-dependencies {
-    'es_extended',
-    'esx_status',
-    'oxmysql'
-}
+dependencies({
+    "es_extended",
+    "esx_status",
+    "oxmysql",
+})
