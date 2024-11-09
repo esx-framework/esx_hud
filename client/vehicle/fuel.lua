849fcb4854505e10b529e00be2e8d23f11a8c984
Kasey FItton <kaseyfitton@hotmail.co.uk>
2024-11-09 02:01:03 +0000

    tweak(client/vehicle): set default fuel

--- client/vehicle/fuel.lua
+++ client/vehicle/fuel.lua
@@ -1,16 +1,11 @@
 if not Config.Disable.Vehicle then
-    if GetResourceState("ox_fuel") == "started" then
-        function HUD:FuelExport()
-            return ESX.Math.Round(GetVehicleFuelLevel(HUD.Data.Vehicle), 2)
-        end
-    elseif GetResourceState("LegacyFuel") == "started" then
+    if GetResourceState("LegacyFuel") == "started" then
         function HUD:FuelExport()
             return ESX.Math.Round(exports["LegacyFuel"]:GetFuel(HUD.Data.Vehicle), 2)
         end
     else
         function HUD:FuelExport()
-            return false
+            return ESX.Math.Round(GetVehicleFuelLevel(HUD.Data.Vehicle), 2)
         end
-        TriggerServerEvent("esx_hud:ErrorHandle", "Setup your custom fuel resource at: client/vehicle/fuel.lua")
     end
 end
